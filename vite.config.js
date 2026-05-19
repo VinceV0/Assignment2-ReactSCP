a551c36bc11df55afda8c8921908adbcc77c42dd
VinceV0 <30036567@student.toiohomai.ac.nz>
2026-05-19 16:28:56 +1200

    Inital commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh' // or @vitejs/plugin-react
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
